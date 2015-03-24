b81e9b596e100bdadf3fdbae2d2dbbab363fa893
Orçun Ege Acar <orcunegeacarx1@hotmail.com>
2015-03-24 19:23:12 +0200

    Save Form  2&1
    Get&Set

---
+++ Information.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dream_Browser
+{
+   public class Information
+    {
+
+       private string data1;
+       private string data2;
+       private string data3;
+
+       public string Data1
+       {
+           get { return data1; }
+           set { data1 = value; }
+       }
+
+       public string Data2
+       {
+           get { return data2; }
+           set { data2 = value; }
+       }
+
+       public string Data3
+       {
+           get { return data3; }
+           set { data3 = value; }
+       }
+
+
+
+    }
+}
